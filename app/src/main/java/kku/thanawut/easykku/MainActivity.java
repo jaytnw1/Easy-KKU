566685d8604d1186402a95a2d311963d46b79f70
Thanawut <thanawut@kkumail.com>
2016-11-12 12:04:56 +0700

    Intent to SignUp Finish

--- app/src/main/java/kku/thanawut/easykku/MainActivity.java
+++ app/src/main/java/kku/thanawut/easykku/MainActivity.java
@@ -1,13 +1,33 @@
 package kku.thanawut.easykku;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private Button signInButton, signUpButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        //Bind Widget
+        signInButton = (Button) findViewById(R.id.button);
+        signUpButton = (Button) findViewById(R.id.button2);
+        // Sing up Controler
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SignUpActivity.class));
+            }
+        });
+
+
+
+    } // Mian Medthod
+} // Main Class
